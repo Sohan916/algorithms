31d762877eaa985404203c09e57dddb6672c0342
Sohan.io <sohan@aereo.io>
2025-10-30 23:25:05 +0530

    Remove unnecessary print statements for user input in containsDuplicate function

--- Arrays & Hashing/Contains Duplicate/code.py
+++ Arrays & Hashing/Contains Duplicate/code.py
@@ -6,10 +6,6 @@ def containsDuplicate(nums):
         numsSet.add(i)
     return False
 
-print('Please enter an array of numbers')
-
-
-
 print(containsDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 print(containsDuplicate([1, 2, 3, 4, 5, 6, 7, 7, 9, 10]))
